18416188667bbeb089271ceb339c1e78747d4a26
Garmdigo <44831968+Garmdigo@users.noreply.github.com>
2019-09-21 20:16:32 -0400

    Working on alias

--- main.c
+++ main.c
@@ -20,40 +20,47 @@ typedef struct
 	char** tokens;
 	int numTokens;
 } instruction;
-void run(instruction * instr_ptr);
+typedef struct
+{
+	char * name[10];
+	char * instruction[10];
+} alias;
+void run(instruction * instr_ptr, int c_alias,  alias * name);
 void addToken(instruction* instr_ptr, char* tok);
 void printTokens(instruction* instr_ptr);
 void clearInstruction(instruction* instr_ptr);
 void addNull(instruction* instr_ptr);
-void piping(char * before,int c_pipe);
+void piping(instruction * instr_ptr,int c_pipe);
 int ErrorCheck( instruction * instr_ptr );
-void OutRedir(char * before);
-void InRedir(char * before);
+void OutRedir(instruction *insr_ptr,int i);
+void InRedir(instruction *insr_ptr,int i);
 void my_execute(char **cmd);
 char *getPrompt();
 char *PATHRes(char *cmd);
+char*getEnviornment(const char*name);
+char *redirectChars = "<>|&";
+
 
 int main()
 {
+	alias store_alias;
 	char* token = NULL;
 	char* temp = NULL;
-
 	instruction instr;
 	instr.tokens = NULL;
 	instr.numTokens = 0;
-
-
+	int c_alias=0;
 	while (1) {
-
 		printf("%s ", getPrompt());
     int valid = 0; //checks if the command is valid
     int c_pipe = 0; //tracks the amount of pipes
-    int c_input = 0;
-    int c_output = 0;
-    int c_fslash = 0;
-    int c_and = 0;
+    // int c_input = 0;
+    // int c_output = 0;
+    // int c_fslash = 0;
+    // int c_and = 0;
       char * after;
       char *before;
+			int correct=0;
 		// loop reads character sequences separated by whitespace
 		do {
 			//scans for next token and allocates token var to size of scanned token
@@ -77,18 +84,19 @@ int main()
           specialChar[1] = '\0';
           addToken(&instr,specialChar);
           start = i + 1;
-           if (token[i] == '|')
+           if (token[i] == '|') // checks if it is a multiple pipeline
           c_pipe++;
-          else if (token[i] == '>')
-            c_input++;
-        else if (token[i] == '<')
-          c_output++;
-        else if (token[i] == '&')
-          c_and++;
-				}
-			}
+        //   else if (token[i] == '>')
+        //     c_input++;
+        // else if (token[i] == '<')
+        //   c_output++;
+        // else if (token[i] == '&')
+        //   c_and++;
+				// }
 
-			if (start < strlen(token)) {
+			}
+		}
+		if (start < strlen(token)) {
 				memcpy(temp, token + start, strlen(token) - start);
 				temp[i-start] = '\0';
 				addToken(&instr, temp);
@@ -103,17 +111,17 @@ int main()
 
 
 		} while ('\n' != getchar());    //until end of line is reached
-    valid=ErrorCheck(&instr);
+    valid=ErrorCheck(&instr); //does the errorchecking
     printf("Validity:%d\n",valid);
-		if(valid)
+		if(valid) //checks if it passed the error checking
 		{
-				run(&instr);
+			 correct++;
+				run(&instr,c_alias,&store_alias);
 		}
 		else
 		{
-		printf("False\n");
+		printf("Invalid Command\n");
 		}
-
 		addNull(&instr);
 		//printTokens(&instr);
 		clearInstruction(&instr);
@@ -160,49 +168,73 @@ void printTokens(instruction* instr_ptr)
 			printf("%s\n", (instr_ptr->tokens)[i]);
 	}
 }
-void run(instruction * instr_ptr)
+void run(instruction * instr_ptr, int c_alias, alias * name)
 {
 	int i;
-	char *cmd1;
-	char *cmd2;
-
 	for (i = 0; i < instr_ptr->numTokens; i++)
 	{
 		if ((instr_ptr->tokens)[i] != NULL)
 			{
 				if(strcmp(instr_ptr->tokens[i], "|")==0)
 				{
-						// strcpy(cmd1,instr_ptr->tokens[i-1]);
-						// strcpy(cmd2,instr_ptr->tokens[i+1]);
-								printf("Hey2\n");
+					 piping(&instr_ptr,i);
 					}
 					else if (strcmp(instr_ptr->tokens[i], "<")==0)
 					{
+									 InRedir(&instr_ptr,i);
 					// strcpy(cmd1,instr_ptr->tokens[i-1]);
 					// strcpy(cmd2,instr_ptr->tokens[i+1]);
-							//printf("hey brah %s:",instr_ptr->tokens[i+2]);
-							// if (instr_ptr->tokens[i+2] == NULL)
-							// {
-							// 	printf("yay\n");
-							// }
-							// else
-							// {
-							printf("NO\n");
-						}
 
-					}
-					 if (strcmp(instr_ptr->tokens[i], ">")==0)
+						}
+					else if (strcmp(instr_ptr->tokens[i], ">")==0)
 					{
+							 OutRedir(&instr_ptr,i);
 					// strcpy(cmd1,instr_ptr->tokens[i-1]);
 					// strcpy(cmd2,instr_ptr->tokens[i+1]);
 					}
 					else if (strcmp(instr_ptr->tokens[i], "&")==0)
 					{
-						// strcpy(cmd1,instr_ptr->tokens[i-1]);
-						// strcpy(cmd2,instr_ptr->tokens[i+1]);
 					}
-					else
+					else if (strcmp(instr_ptr->tokens[i], "alias")==0)
 					{
+						char *string;
+						string=instr_ptr->tokens[i+1];
+  					char *p,*q;
+  					printf ("String  \"%s\" is split into tokens:\n",string);
+  					p = strtok (string,"=");
+
+  					while (p!= NULL)
+  					{
+    				printf ("%s\n",p);
+    			  q= strtok (NULL, "'");
+						printf ("%s\n",q);
+						break;
+  					}
+						// printf("hEEEERRE");
+						// char * token=strtok(instr_ptr->tokens[i+1], "=");
+						// char * token2=strtok(instr_ptr->tokens[i+1], "'");
+						// char * token3=strtok(instr_ptr->tokens[i+1], "'");
+						// printf("First:%s\nSecond:%s\nThird:%s\n",token,token2,token3 );
+					}
+						if (strcmp(instr_ptr->tokens[i], "echo")==0&&instr_ptr->tokens[i+1] != NULL)
+							{
+								;
+								// int k=1;
+								// 	for(k;k<instr_ptr->numTokens;k++)
+								// 	{
+								// 		if ((instr_ptr->tokens)[k] != NULL)
+								// 		{
+								//
+ 							 	// 		printf("%s\n",getEnviornment((instr_ptr->tokens)[k]));
+								// 		}
+								//
+								// 	}
+							}
+							else if(instr_ptr->tokens[i+1] == NULL)
+							{
+								printf("\n"); //if you type echo, it shows  a new line
+							}
+
 						// printf("Enter %s\n",instr_ptr->tokens[i]);
 					}
 
@@ -214,37 +246,46 @@ int ErrorCheck( instruction * instr_ptr )
   int i; //for loop
   for (i = 0; i < instr_ptr->numTokens; i++)
    {
-    if ((instr_ptr->tokens)[i] != NULL)
+    if ((instr_ptr->tokens)[i] != NULL) //checks if null
     {
       if (i==0&&(strcmp(instr_ptr->tokens[i], "|")==0
       ||strcmp(instr_ptr->tokens[i], "<")==0||strcmp((instr_ptr->tokens)[i], ">")==0
-      ||strcmp((instr_ptr->tokens)[i], "&")==0))
+      )) //if the first input is valid
       {
-        return 0;
+				  if ((instr_ptr->tokens)[1] != NULL)
+					{
+						if(strcmp(instr_ptr->tokens[i], "&")==0&&(strcmp(instr_ptr->tokens[i+1], "|")==0
+	      			||strcmp(instr_ptr->tokens[i+1], "<")==0||strcmp((instr_ptr->tokens)[i+1], ">")==0))
+							return 0;
+						}
+        	return 0;
       }
-     else if ((strcmp((instr_ptr->tokens)[i], "|")==0 ||strcmp((instr_ptr->tokens)[i], "<")==0||strcmp((instr_ptr->tokens)[i], ">")==0||strcmp((instr_ptr->tokens)[i], "&")== 0))
+     else if ((strcmp((instr_ptr->tokens)[i], "|")==0 ||strcmp((instr_ptr->tokens)[i], "<")==0||strcmp((instr_ptr->tokens)[i], ">")==0)) //||strcmp((instr_ptr->tokens)[i], "&")==0)
      {
 				int j=i+1;
-        if(instr_ptr->tokens[j]!=NULL)
+        if(instr_ptr->tokens[j]!=NULL)// checking if the next input is valid or not
          {
-          if ((strcmp((instr_ptr->tokens)[j], "|")==0 ||strcmp((instr_ptr->tokens)[j], "<")==0||strcmp((instr_ptr->tokens)[j], ">")==0||strcmp((instr_ptr->tokens)[j], "&")== 0))
+          if ((strcmp((instr_ptr->tokens)[j], "|")==0 ||strcmp((instr_ptr->tokens)[j], "<")==0||strcmp((instr_ptr->tokens)[j], ">")==0)) //||strcmp((instr_ptr->tokens)[j], "&")==0)
           {
-          return 0;
+          return 0; //its false
         }
         }
+				// else if (strcmp(instr_ptr->tokens[i], "&")==0&&strcmp(instr_ptr->tokens[j], "&")!=0)
+				// {
+				// 	return 1;
+				// }
         else
         {
-          return 0;
+          return 0; //false
         }
       }
   	}
     else
     {
     return 0;
-
   }
   }
-return 1;
+return 1;//true
 }
 
 void clearInstruction(instruction* instr_ptr)
@@ -258,31 +299,30 @@ void clearInstruction(instruction* instr_ptr)
 	instr_ptr->tokens = NULL;
 	instr_ptr->numTokens = 0;
 }
-void InRedir(char * before)
+void InRedir(instruction * instr_ptr, int i)
 {
-	int fd= open(before,O_RDONLY);
+	int fd= open(instr_ptr->tokens[i+1],O_RDONLY);
 	if(fork()==0)
 	{
 		close (STDIN_FILENO);
 		dup(fd);
 		close(fd);
-  my_execute
+   my_execute(instr_ptr->tokens);
 	}
 	else
 	{
 		close (fd);
 	}
 }
-void OutRedir(char * before)
+void OutRedir(instruction * instr_ptr,int i)
 {
-	int fd=open(before,O_RDONLY|O_CREAT|O_TRUNC);
+	int fd=open(instr_ptr->tokens[i+1],O_RDONLY|O_CREAT|O_TRUNC);
 	if (fork()==0)
 	{
 		close(STDOUT_FILENO);
 		dup(fd);
 		close(fd);
-    //execute
-    exit(fd);
+    my_execute(instr_ptr->tokens);
 	}
 	else
 	{
@@ -290,11 +330,12 @@ void OutRedir(char * before)
 	}
 }
 
-void piping(char * before, int c_pipe)
+void piping(instruction * instr_ptr, int c_pipe)
 {
 	int fd[2];
-
-	if (fork()==0)
+	int status;
+	pid_t pid = fork();
+	if (pid==0)
 	{
     if(c_pipe==1) //single pipeline
   	{
@@ -305,7 +346,7 @@ void piping(char * before, int c_pipe)
 			dup(fd[1]);
 			close (fd[0]);
 			close (fd[1]);
-      //execute
+      my_execute(instr_ptr->tokens);
 		}
 		else
 		{
@@ -313,11 +354,12 @@ void piping(char * before, int c_pipe)
 			dup(fd[0]);
 			close (fd[0]);
 			close (fd[1]);
-      //execute
+  		my_execute(instr_ptr->tokens);
 		}
   }
 	else if (c_pipe>1) //multipiping
 	{
+
 			pipe(fd);
 			if(fork()==0)
 			{
@@ -353,7 +395,7 @@ void piping(char * before, int c_pipe)
   }
 	else
 	{
-    waitpid(NULL);
+      waitpid(pid, &status, 0);
 
 	}
 }
@@ -403,12 +445,12 @@ char *getPrompt() {
 	strcat(prompt, hostname);
 	strcat(prompt, ":");
 	strcat(prompt, dir);
-	strcat(prompt, ">");
+	strcat(prompt, " >");
 
 	return prompt;
 
 }
-char *redirectChars = "<>|&";
+
 
 char *PATHRes(char *cmd){
 
@@ -461,4 +503,41 @@ char *PATHRes(char *cmd){
     printf("Command not found");
     return NULL;
 
+}
+char*getEnviornment(const char*name)
+{
+
+    char *v = NULL;
+    char *result = (char *)malloc(100 * sizeof(char));
+
+    if (name == NULL)
+    {
+        return NULL;
+    }
+
+    else {
+        if (strcmp(name, "$PATH") == 0){
+            v = getenv( "PATH" );
+
+        }
+        else if (strcmp(name, "~") == 0){
+            v = getenv( "HOME" );
+
+        }
+        else if (strcmp(name, "$USER") == 0){
+            v = getenv( "USER" );
+
+        }
+        else if (strcmp(name, "$SHELL") == 0){
+            v = getenv( "SHELL" );
+
+        }
+        else if (strcmp(name, "$PWD") == 0){
+            v = getenv( "PWD" );
+
+        }
+
+    }
+    strcpy(result, v);
+    return  result;
 }
